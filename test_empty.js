b4b89ff6b05647d9e42d03ff760a2a899db2f405
Allen Contreras <allenyair01@gmail.com>
2026-07-08 13:57:07 -0600

    Remove test scripts

--- test_empty.js
+++
@@ -1,7 +0,0 @@
-const sharp = require('sharp');
-async function test() {
-    const emptySvg = `<svg width="400" height="200" xmlns="http://www.w3.org/2000/svg"></svg>`;
-    const b = await sharp(Buffer.from(emptySvg)).png().toBuffer();
-    console.log("Empty length:", b.length);
-}
-test();
